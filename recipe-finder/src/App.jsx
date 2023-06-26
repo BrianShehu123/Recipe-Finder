cd4b81c679eef71d863a95f8913ee0084754d2ff
Brian Shehu <brian.shehu2@gmail.com>
2023-06-26 00:10:24 -0400

    added the components

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -1,1 +1,18 @@
-import NavBar from './components'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import HomePage from './components/HomePage'
+
+function App() {
+
+  return (
+  <div>
+
+    <NavBar />
+    <HomePage />
+    <Footer />
+
+  </div>
+  )
+}
+
+export default App
